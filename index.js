653bffb86ace7bf2113cf88a5a09083d205b42d6
himu62 <zero.256dic@gmail.com>
2016-10-11 15:46:46 +0900

    human visible

--- index.js
+++ index.js
@@ -32,21 +32,21 @@ $(function() {
 
 		switch(max) {
 			case scores.anger:
-				return 'anger ' + scores.anger.toFixed(3);
+				return 'anger ' + scores.anger.toFixed(4);
 			case scores.contempt:
-				return 'contempt ' + scores.contempt.toFixed(3);
+				return 'contempt ' + scores.contempt.toFixed(4);
 			case scores.disgust:
-				return 'disgust ' + scores.disgust.toFixed(3);
+				return 'disgust ' + scores.disgust.toFixed(4);
 			case scores.fear:
-				return 'fear ' + scores.fear.toFixed(3);
+				return 'fear ' + scores.fear.toFixed(4);
 			case scores.happiness:
-				return 'happiness ' + scores.happiness.toFixed(3);
+				return 'happiness ' + scores.happiness.toFixed(4);
 			case scores.neutral:
-				return 'neutral ' + scores.neutral.toFixed(3);
+				return 'neutral ' + scores.neutral.toFixed(4);
 			case scores.sadness:
-				return 'sadness ' + scores.sadness.toFixed(3);
+				return 'sadness ' + scores.sadness.toFixed(4);
 			case scores.surprise:
-				return 'surprise ' + scores.surprise.toFixed(3);
+				return 'surprise ' + scores.surprise.toFixed(4);
 			default:
 				return 'none';
 		}
@@ -73,7 +73,7 @@ $(function() {
 			data: file,
 			dataType: 'json',
 			headers: {
-				'Ocp-Apim-Subscription-Key': 'api key here'
+				'Ocp-Apim-Subscription-Key': 'subscription key here'
 			},
 			method: 'POST',
 			processData: false,
@@ -85,10 +85,15 @@ $(function() {
 
 			for(var i in data) {
 				var rect = data[i].faceRectangle;
+				var text = MaxEmotion(data[i].scores);
 
 				ctx.beginPath();
 				ctx.strokeRect(rect.left, rect.top, rect.width, rect.height);
-				ctx.fillText(MaxEmotion(data[i].scores), rect.left, rect.top - 12);
+				ctx.fillStyle = 'black';
+				var m = ctx.measureText(text);
+				ctx.fillRect(rect.left, rect.top - 10, m.width, 10);
+				ctx.fillStyle = 'white';
+				ctx.fillText(text, rect.left, rect.top);
 			}
 		})
 		.fail(function(error) {
